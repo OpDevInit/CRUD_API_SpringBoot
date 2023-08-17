b6e03790f4d96be977193b092622947187cd840d
jholl <jholzinhooh567@gmail.com>
2023-08-17 19:12:44 -0300

    Task Services Created

---
+++ src/main/java/com/teste/teste_spring/services/TaskService.java
@@ -0,0 +1,55 @@
+package com.teste.teste_spring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teste.teste_spring.models.Task;
+import com.teste.teste_spring.models.User;
+import com.teste.teste_spring.repositories.TaskRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserServices userServices;
+
+    public Task findById(Long id){
+        Optional<Task> task = this.taskRepository.findById(id);
+        return task.orElseThrow(()-> new RuntimeException(
+            "Tarefa não encontrada!Id: "+id+", Tipo: "+Task.class.getName()
+            ));
+    }
+
+    @Transactional
+    public Task create(Task obj){
+        User user = this.userServices.findById(obj.getUser().getId());
+        obj.setId(null);
+        obj.setUser(user);
+        obj = this.taskRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Task update(Task obj){
+        Task newObj = findById(obj.getId());
+        newObj.setDescription(obj.getDescription());
+        return this.taskRepository.save(newObj);
+    }
+
+    public void delete(Long id){
+        Task task = findById(id);
+        try {
+            this.taskRepository.delete(task);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possivel excluir,"
+            + "há entidades relacionadas! Id: " + id + " , Tipo: " + User.class.getName());
+        }
+    }
+}
--- src/main/java/com/teste/teste_spring/services/UserServices.java
+++ src/main/java/com/teste/teste_spring/services/UserServices.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.teste.teste_spring.models.User;
-import com.teste.teste_spring.repositories.TaskRepository;
 import com.teste.teste_spring.repositories.UserRepository;
 
 import jakarta.transaction.Transactional;
@@ -19,7 +18,6 @@ public class UserServices {
     private UserRepository userRepository;
 
     @Autowired
-    private TaskRepository taskRepository;
 
     public User findById(Long id) {
 
@@ -29,30 +27,28 @@ public class UserServices {
     }
 
     @Transactional
-
     public User create(User obj) {
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTask());
         return obj;
 
     }
+
     @Transactional
-    public User update(User obj){
-       User newObj = findById(obj.getId());
-       newObj.setPassword((obj.getPassword()));
-       return this.userRepository.save(newObj); 
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword((obj.getPassword()));
+        return this.userRepository.save(newObj);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         findById(id);
         try {
             this.userRepository.deleteById(id);
         } catch (Exception e) {
-            throw new RuntimeException("Não é possivel excluir,  há entidades relacionadas!");
+            throw new RuntimeException("Não é possivel excluir,"
+            + "há entidades relacionadas! Id: " + id + " , Tipo: " + User.class.getName());
         }
     }
-
-
 
 }
